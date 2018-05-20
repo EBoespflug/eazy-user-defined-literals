3762d439aaf38ff04519e2457630929815140bb9
= <etienne.boespflug@gmail.com>
2018-05-20 06:14:57 +0200

    ezl.hpp refining.

--- src/ezl.hpp
+++ src/ezl.hpp
@@ -1,5 +1,5 @@
-#ifndef LIB_EAZY_USERDEFINED_LITERALS
-#define LIB_EAZY_USERDEFINED_LITERALS
+#ifndef EAZY_USERDEFINED_LITERALS
+#define EAZY_USERDEFINED_LITERALS
 
 #include <cstddef>
 
@@ -11,27 +11,29 @@
  * __VA_ARGS__ : macro call_ called with those args. use EZL_MAKE_STR_CPL if no * args.
  */
 #define EZL_MAKE_STR_CPLVA(specifiers_, name_, call_, ...) \
-     specifiers_ auto operator "" \
-     EZL_STRINGCAT(_, name_)(const char* str, [[maybe_unused]] std::size_t size) { \
-         call_(__VA_ARGS__) \
-     }
+    specifiers_ auto operator "" \
+    EZL_STRINGCAT(_, name_)(const char* str, [[maybe_unused]] std::size_t size) { \
+        call_(__VA_ARGS__) \
+    }
 
- #define EZL_MAKE_STR_CPL(specifiers_, name_, call_) \
-      specifiers_ auto operator "" \
-      EZL_STRINGCAT(_, name_)(const char* str, [[maybe_unused]] std::size_t size) { \
-          call_() \
-      }
+#define EZL_MAKE_STR_CPL(specifiers_, name_, call_) \
+    specifiers_ auto operator "" \
+    EZL_STRINGCAT(_, name_)(const char* str, [[maybe_unused]] std::size_t size) { \
+      call_() \
+    }
 
-#define EZL_MAKE_STR_(specifier_, type_, name_) \
-     specifiers_ auto operator "" \
-     EZL_STRINGCAT(_, name_)const char* str, [[maybe_unused]] std::size_t size) { \
-         return type_{str}; \
-     }
+#define EZL_MAKE_STR_CPL_D(specifiers_, name_, call_) \
+    specifiers_ auto operator "" \
+    EZL_STRINGCAT(_, name_)(const char* str, [[maybe_unused]] std::size_t size) { \
+       call_ \
+    }
 
-#define EZL_MAKE_STR(type_, name_) EZL_MAKE_STR_EDL(inline, type_, name_)
-#define EZL_MAKE_STR_C(type_, name_) EZL_MAKE_STR_EDL(constexpr, type_, name_)
+#define EZL_MAKE_STR_(specifiers_, type_, name_) EZL_MAKE_STR_CPL_D(specifiers_, name_, type_{str};)
+
+#define EZL_MAKE_STR(type_, name_) EZL_MAKE_STR_(inline, type_, name_)
+#define EZL_MAKE_STR_C(type_, name_) EZL_MAKE_STR_(constexpr, type_, name_)
 
 #define EZL_MAKE(type_, name_) EZL_MAKE_STR(type_, name_)
 #define EZL_MAKE_C(type_, name_) EZL_MAKE_STR_C(type_, name_)
 
-#endif // LIB_EAZY_USERDEFINED_LITERALS
+#endif // EAZY_USERDEFINED_LITERALS
