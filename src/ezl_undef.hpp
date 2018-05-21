e3ee4756c1d81ce630d0043cf915c31434551ad8
= <etienne.boespflug@gmail.com>
2018-05-21 12:27:08 +0200

    Undef corrections.

--- src/ezl_undef.hpp
+++ src/ezl_undef.hpp
@@ -1,1 +1,10 @@
 #undef EAZY_USERDEFINED_LITERALS
+
+#include "ezl_util_undef.hpp"
+#include "ezl_raw_undef.hpp"
+#include "ezl_num_undef.hpp"
+#include "ezl_char_undef.hpp"
+#include "ezl_str_undef.hpp"
+
+#undef EZL_MAKE
+#undef EZL_MAKE_C
