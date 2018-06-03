74f223c635955a8ea4d0d7be8db26c73d690a27b
= <etienne.boespflug@gmail.com>
2018-06-03 23:34:01 +0200

    Added documentation for ezl_num.hpp.

--- src/ezl_num.hpp
+++ src/ezl_num.hpp
@@ -20,30 +20,176 @@
 #define EAZY_USERDEFINED_LITERALS_NUM
 
 // unsigned long long int
+/**
+ * Creates an integer user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (call_, which is expanded with the variadic arguments).
+ *
+ * The argument of the UDL operator is a (of type unsigned long long).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param call_         The function macro to be expanded
+ *      with the variadic argument.
+ *
+ * @see EZL_MAKE_GEN_CPL
+ */
 #define EZL_MAKE_INT_CPL(specifiers_, name_, call_, ...) \
     EZL_MAKE_GEN_CPL(specifiers_, name_, unsigned long long int, call_, __VA_ARGS__)
 
-#define EZL_MAKE_INT_CPL_D(specifiers_, name_, call_) \
-    EZL_MAKE_GEN_CPL_D(specifiers_, name_, unsigned long long int, call_)
+/**
+ * Creates an integer user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (body_, which is a direct preprocessor replacement).
+ *
+ * The argument of the UDL operator is a (of type unsigned long long).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param body_         The function macro to be expanded
+ *      with the variadic argument.
+ *
+ * @see EZL_MAKE_GEN_CPL_D
+ */
+#define EZL_MAKE_INT_CPL_D(specifiers_, name_, body_) \
+    EZL_MAKE_GEN_CPL_D(specifiers_, name_, unsigned long long int, body_)
 
+/**
+ * Creates an integer user-defined literal with the specifiers_
+ * and the name_ (prefixed with an underscore) converting the
+ * integer literal argument in type_.
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the integer literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_INT_CPL_D
+ */
 #define EZL_MAKE_INT(specifiers_, type_, name_) EZL_MAKE_INT_CPL_D(specifiers_, name_, return type_(a);)
 
+/**
+ * Creates an inline integer user-defined literal with the specifiers_
+ * and the name_ (prefixed with an underscore) converting the integer
+ * literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_INT with inline
+ * specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the integer literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_INT
+ */
 #define EZL_MAKE_INT_I(type_, name_) EZL_MAKE_INT(inline, type_, name_)
+/**
+ * Creates a constexpr integer user-defined literal with the specifiers_
+ * and the name_ (prefixed with an underscore) converting the integer
+ * literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_INT with constexpr
+ * specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the integer literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_INT
+ */
 #define EZL_MAKE_INT_C(type_, name_) EZL_MAKE_INT(constexpr, type_, name_)
 
 // long double
-#define EZL_MAKE_FLOAT_CPL_VA(specifiers_, name_, call_, ...) \
-    EZL_MAKE_GEN_CPL_VA(specifiers_, name_, long double, call_, __VA_ARGS__)
 
-#define EZL_MAKE_FLOAT_CPL(specifiers_, name_, call_) \
-    EZL_MAKE_GEN_CPL(specifiers_, name_, long double, call_)
+/**
+ * Creates an floating point user-defined literal with the
+ * specifiers_, the name_ (prefixed with an underscore) and
+ * body instructions (call_, which is expanded with the
+ * variadic arguments).
+ *
+ * The argument of the UDL operator is a (of type long double).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param call_         The function macro to be expanded
+ *      with the variadic argument.
+ *
+ * @see EZL_MAKE_GEN_CPL
+ */
+#define EZL_MAKE_FLOAT_CPL(specifiers_, name_, call_, ...) \
+    EZL_MAKE_GEN_CPL(specifiers_, name_, long double, call_, __VA_ARGS__)
 
-#define EZL_MAKE_FLOAT_CPL_D(specifiers_, name_, call_) \
-    EZL_MAKE_GEN_CPL_D(specifiers_, name_, long double, call_)
+/**
+ * Creates a floating-point user-defined literal with the
+ * specifiers_, the name_ (prefixed with an underscore) and
+ * body instructions (body_, which is a direct preprocessor
+ * replacement).
+ *
+ * The argument of the UDL operator is a (of type long double).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param body_         The function macro to be expanded
+ *      with the variadic argument.
+ *
+ * @see EZL_MAKE_GEN_CPL_D
+ */
+#define EZL_MAKE_FLOAT_CPL_D(specifiers_, name_, body_) \
+    EZL_MAKE_GEN_CPL_D(specifiers_, name_, long double, body_)
 
-#define EZL_MAKE_FLOAT(specifiers_, type_, name_) EZL_MAKE_FLOAT_CPL_D(specifiers_, name_, type_{a};)
+/**
+ * Creates a floating-point user-defined literal with the
+ * specifiers_ and the name_ (prefixed with an underscore)
+ * converting the floating-point literal argument in type_.
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the floating-point
+ *      literal will be converted.
+ *
+ * @see EZL_MAKE_FLOAT_CPL_D
+ */
+#define EZL_MAKE_FLOAT(specifiers_, type_, name_) EZL_MAKE_FLOAT_CPL_D(specifiers_, name_, type_(a);)
 
+/**
+ * Creates an inline floating-point user-defined literal with the
+ * specifiers_ and the name_ (prefixed with an underscore) converting
+ * the floating-point literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_FLOAT with inline
+ * specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the floating-point
+ *      literal will be converted.
+ *
+ * @see EZL_MAKE_FLOAT
+ */
 #define EZL_MAKE_FLOAT_I(type_, name_) EZL_MAKE_FLOAT(inline, type_, name_)
-#define EZL_MAKE_FLOAT_C(type_, name_) EZL_MAKE_FLOAT(constexpr, type_, name_)
+/**
+ * Creates a constexpr floating-point user-defined literal with the
+ * specifiers_ and the name_ (prefixed with an underscore) converting
+ * the floating-point literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_FLOAT with constexpr
+ * specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the floating-point
+ *      literal will be converted.
+ *
+ * @see EZL_MAKE_FLOAT
+ */
+ #define EZL_MAKE_FLOAT_C(type_, name_) EZL_MAKE_FLOAT(constexpr, type_, name_)
 
 #endif // EAZY_USERDEFINED_LITERALS_NUM
--- src/ezl_str.hpp
+++ src/ezl_str.hpp
@@ -2,6 +2,9 @@
  * ezl_str.hpp
  * Etienne Boespflug - 2018
  *
+ * This file contains the strings function macros for
+ * creating user-defined literals (EZL_MAKE_STR*).
+ *
  * This software has no license, feel free to use,
  * modify and distribute.
  *
@@ -23,51 +26,355 @@
 #include <cstddef>
 
 // (const char*, std::size_t)
+
+/**
+ * Creates a string user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (call_, which is expanded with the variadic arguments).
+ *
+ * The arguments of the UDL operator are a (of type const char*)
+ * and b (of type std::size_t).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param call_         The function macro to be expanded
+ *      with the variadic argument.
+ *
+ * @see EZL_MAKE_GEN2_CPL
+ */
 #define EZL_MAKE_STR_CPL(specifiers_, name_, call_, ...) \
     EZL_MAKE_GEN2_CPL(specifiers_, name_, const char*, std::size_t, call_, __VA_ARGS__)
 
-#define EZL_MAKE_STR_CPL_D(specifiers_, name_, call_) \
-    EZL_MAKE_GEN2_CPL_D(specifiers_, name_, const char*, std::size_t, call_)
+/**
+ * Creates a string user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (body_, which is a direct preprocessor replacement).
+ *
+ * The arguments of the UDL operator are a (of type const char*)
+ * and b (of type std::size_t).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param body_         The macro constituting the body
+ *      argument.
+ *
+ * @see EZL_MAKE_GEN2_CPL_D
+ */
+#define EZL_MAKE_STR_CPL_D(specifiers_, name_, body_) \
+    EZL_MAKE_GEN2_CPL_D(specifiers_, name_, const char*, std::size_t, body_)
 
+/**
+ * Creates a string user-defined literal with the specifiers_
+ * and the name_ (prefixed with an underscore) converting the
+ * string literal argument in type_.
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR_CPL_D
+ */
 #define EZL_MAKE_STR(specifiers_, type_, name_) EZL_MAKE_STR_CPL_D(specifiers_, name_, return type_(a);)
 
+/**
+ * Creates an inline string user-defined literal with the
+ * specifiers_ and the name_ (prefixed with an underscore)
+ * converting the string literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_STR
+ * with inline specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR
+ */
 #define EZL_MAKE_STR_I(type_, name_) EZL_MAKE_STR(inline, type_, name_)
+
+/**
+ * Creates a constexpr string user-defined literal with the
+ * specifiers_ and the name_ (prefixed with an underscore)
+ * converting the string literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_STR
+ * with constexpr specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR
+ */
 #define EZL_MAKE_STR_C(type_, name_) EZL_MAKE_STR(constexpr, type_, name_)
 
 // (const wchar_t*, std::size_t)
+
+/**
+ * Creates a wide-string user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (call_, which is expanded with the variadic arguments).
+ *
+ * The arguments of the UDL operator are a (of type const wchar_t*)
+ * and b (of type std::size_t).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param call_         The function macro to be expanded
+ *      with the variadic argument.
+ *
+ * @see EZL_MAKE_GEN2_CPL
+ */
 #define EZL_MAKE_WSTR_CPL(specifiers_, name_, call_, ...) \
     EZL_MAKE_GEN2_CPL(specifiers_, name_, const wchar_t*, std::size_t, call_, __VA_ARGS__)
 
-#define EZL_MAKE_WSTR_CPL_D(specifiers_, name_, call_) \
-    EZL_MAKE_GEN2_CPL_D(specifiers_, name_, const wchar_t*, std::size_t, call_)
+/**
+ * Creates a wide-string user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (body_, which is a direct preprocessor replacement).
+ *
+ * The arguments of the UDL operator are a (of type const wchar_t*)
+ * and b (of type std::size_t).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param body_         The macro constituting the body
+ *      argument.
+ *
+ * @see EZL_MAKE_GEN2_CPL_D
+ */
+#define EZL_MAKE_WSTR_CPL_D(specifiers_, name_, body_) \
+    EZL_MAKE_GEN2_CPL_D(specifiers_, name_, const wchar_t*, std::size_t, body_)
 
-#define EZL_MAKE_WSTR(specifiers_, type_, name_) EZL_MAKE_WSTR_CPL_D(specifiers_, name_, type_{a};)
+/**
+ * Creates a wide-string user-defined literal with the specifiers_
+ * and the name_ (prefixed with an underscore) converting the
+ * string literal argument in type_.
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_WSTR_CPL_D
+ */
+#define EZL_MAKE_WSTR(specifiers_, type_, name_) EZL_MAKE_WSTR_CPL_D(specifiers_, name_, type_(a);)
 
+/**
+ * Creates an inline wide-string user-defined literal with
+ * the specifiers_ and the name_ (prefixed with an underscore)
+ * converting the string literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_WSTR
+ * with inline specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_WSTR
+ */
 #define EZL_MAKE_WSTR_I(type_, name_) EZL_MAKE_WSTR(inline, type_, name_)
+/**
+ * Creates a constexpr wide-string user-defined literal with
+ * the specifiers_ and the name_ (prefixed with an underscore)
+ * converting the string literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_WSTR
+ * with constexpr specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_WSTR
+ */
 #define EZL_MAKE_WSTR_C(type_, name_) EZL_MAKE_WSTR(constexpr, type_, name_)
 
 // (const char16_t*, std::size_t)
+/**
+ * Creates a char16_t string user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (call_, which is expanded with the variadic arguments).
+ *
+ * The arguments of the UDL operator are a (of type const char16_t*)
+ * and b (of type std::size_t).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param call_         The function macro to be expanded
+ *      with the variadic argument.
+ *
+ * @see EZL_MAKE_GEN2_CPL
+ */
 #define EZL_MAKE_STR16_CPL(specifiers_, name_, call_, ...) \
     EZL_MAKE_GEN2_CPL(specifiers_, name_, const char16_t*, std::size_t, call_, __VA_ARGS__)
 
-#define EZL_MAKE_STR16_CPL_D(specifiers_, name_, call_) \
-    EZL_MAKE_GEN2_CPL_D(specifiers_, name_, const char16_t*, std::size_t, call_)
+/**
+ * Creates a char16_t string user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (body_, which is a direct preprocessor replacement).
+ *
+ * The arguments of the UDL operator are a (of type const char16_t*)
+ * and b (of type std::size_t).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param body_         The macro constituting the body
+ *      argument.
+ *
+ * @see EZL_MAKE_GEN2_CPL_D
+ */
+#define EZL_MAKE_STR16_CPL_D(specifiers_, name_, body_) \
+    EZL_MAKE_GEN2_CPL_D(specifiers_, name_, const char16_t*, std::size_t, body_)
 
-#define EZL_MAKE_STR16(specifiers_, type_, name_) EZL_MAKE_STR16_CPL_D(specifiers_, name_, type_{a};)
+/**
+ * Creates a char16_t string user-defined literal with the specifiers_
+ * and the name_ (prefixed with an underscore) converting the
+ * string literal argument in type_.
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR16_CPL_D
+ */
+#define EZL_MAKE_STR16(specifiers_, type_, name_) EZL_MAKE_STR16_CPL_D(specifiers_, name_, type_(a);)
 
+/**
+ * Creates an inline char16_t string user-defined literal with
+ * the specifiers_ and the name_ (prefixed with an underscore)
+ * converting the string literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_STR16
+ * with inline specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR16
+ */
 #define EZL_MAKE_STR16_I(type_, name_) EZL_MAKE_STR16(inline, type_, name_)
+/**
+ * Creates a constexpr char16_t string user-defined literal with
+ * the specifiers_ and the name_ (prefixed with an underscore)
+ * converting the string literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_STR16
+ * with constexpr specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR16
+ */
 #define EZL_MAKE_STR16_C(type_, name_) EZL_MAKE_STR16(constexpr, type_, name_)
 
 // (const char32_t*, std::size_t)
+/**
+ * Creates a char32_t string user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (call_, which is expanded with the variadic arguments).
+ *
+ * The arguments of the UDL operator are a (of type const char32_t*)
+ * and b (of type std::size_t).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param call_         The function macro to be expanded
+ *      with the variadic argument.
+ *
+ * @see EZL_MAKE_GEN2_CPL
+ */
 #define EZL_MAKE_STR32_CPL(specifiers_, name_, call_, ...) \
     EZL_MAKE_GEN2_CPL(specifiers_, name_, const char32_t*, std::size_t, call_, __VA_ARGS__)
 
-#define EZL_MAKE_STR32_CPL_D(specifiers_, name_, call_) \
-    EZL_MAKE_GEN2_CPL_D(specifiers_, name_, const char32_t*, std::size_t, call_)
+/**
+ * Creates a char32_t string user-defined literal with the specifiers_,
+ * the name_ (prefixed with an underscore) and body instructions
+ * (body_, which is a direct preprocessor replacement).
+ *
+ * The arguments of the UDL operator are a (of type const char32_t*)
+ * and b (of type std::size_t).
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param body_         The macro constituting the body
+ *      argument.
+ *
+ * @see EZL_MAKE_GEN2_CPL_D
+ */
+#define EZL_MAKE_STR32_CPL_D(specifiers_, name_, body_) \
+    EZL_MAKE_GEN2_CPL_D(specifiers_, name_, const char32_t*, std::size_t, body_)
 
-#define EZL_MAKE_STR32(specifiers_, type_, name_) EZL_MAKE_STR32_CPL_D(specifiers_, name_, type_{a};)
+/**
+ * Creates a char32_t string user-defined literal with the specifiers_
+ * and the name_ (prefixed with an underscore) converting the
+ * string literal argument in type_.
+ *
+ * @param specifiers_   The specifiers of the UDL operator.
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR32_CPL_D
+ */
+#define EZL_MAKE_STR32(specifiers_, type_, name_) EZL_MAKE_STR32_CPL_D(specifiers_, name_, type_(a);)
 
+/**
+ * Creates an inline char32_t string user-defined literal with
+ * the specifiers_ and the name_ (prefixed with an underscore)
+ * converting the string literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_STR32
+ * with inline specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR32
+ */
 #define EZL_MAKE_STR32_I(type_, name_) EZL_MAKE_STR32(inline, type_, name_)
+
+/**
+ * Creates a constexpr char32_t string user-defined literal with
+ * the specifiers_ and the name_ (prefixed with an underscore)
+ * converting the string literal argument in type_.
+ *
+ * Helper function macro corresponding to EZL_MAKE_STR32
+ * with constexpr specifier.
+ *
+ * @param name_         The name of the literal suffix (an
+ *      underscore prefix is added to the name).
+ * @param type_         The type in which the string literal
+ * will be converted.
+ *
+ * @see EZL_MAKE_STR32
+ */
 #define EZL_MAKE_STR32_C(type_, name_) EZL_MAKE_STR32(constexpr, type_, name_)
 
 #endif // EAZY_USERDEFINED_LITERALS_STR
